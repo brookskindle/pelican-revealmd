2df2ae363a812ff4e223da2d42d69917c74d9102
Brooks Kindle <brookskindle+git@gmail.com>
2018-08-08 23:55:34 -0700

    Alpha release of plugin
    It works for my personal blog, but that's only because there are some
    undocumented config settings and theme hacks I had to do for this to
    work.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,10 @@
+import os
+
+from markdown import Markdown
 from pelican import signals
 from pelican.readers import BaseReader, MarkdownReader
+import pypandoc
+
 
 class RevealJSMarkdownReader(BaseReader):
     """Reads a markdown document and converts it to a reveal.js presentation
@@ -15,20 +20,33 @@ class RevealJSMarkdownReader(BaseReader):
         Requires pypandoc (https://github.com/bebraw/pypandoc) and pandoc
         (https://pandoc.org/) to work correctly.
         """
-        import datetime
 
-        print(f"revealjsmarkdownreader reading {filename}")
         # TODO: use markdown reader to parse the reveal.js markdown
         # https://github.com/danielfrg/pelican-ipynb/blob/master/markup.py#L62
-        content, metadata = MarkdownReader(self.settings).read(filename)
-        print(f"metadata is {metadata}")
+        reader = MarkdownReader(self.settings)
+        md_content, metadata = reader.read(filename)
+        metadata["template"] = "blank"
 
-        # content = "<p>These are sample contents. Hello!</p>"
-        metadata = {
-            "title": "test reader title",
-            "date": datetime.datetime(2018, 8, 1),
-        }
-        return (content, metadata)
+        # TODO: using the markdown reader converts the file contents to HTML,
+        # but we just want plain text because pandoc should be converting it
+        # instead. The trouble is, we also want to get the metadata
+
+        md_converter = Markdown(**self.settings["MARKDOWN"])
+        md_converter.convertFile(filename, output=os.devnull)
+
+        md_content = "\n".join(md_converter.lines)
+        # metadata = getattr(md_converter, "Meta", {})
+
+        revealjs_content = pypandoc.convert_text(md_content, to="revealjs",
+            format="md",
+            extra_args=[
+                "-s",
+                "-V", "revealjs-url=https://lab.hakim.se/reveal-js",
+                # "--slide-level", "2",
+            ],
+        )
+
+        return (revealjs_content, metadata)
 
 
 def add_reader(readers):
